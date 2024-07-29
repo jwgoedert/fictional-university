0540b6319e24202bbb5463febd38d693e0fda0b4
James Wesley Goedert <jwgoedert@gmail.com>
2024-07-29 22:46:49 +0200

    Remove test functions

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,17 +1,5 @@
 <?php get_header(); ?>
-<?php 
-    function doubleMe ($num) {
-        $result = $num * 2;
-        return $result;
-    }
-    
-    function tripleMe ($num) {
-        $result = $num * 3;
-        return $result;
-    }
 
-    echo tripleMe(doubleMe(5));
-?>
 <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg')?>)"></div>
       <div class="page-banner__content container t-center c-white">
