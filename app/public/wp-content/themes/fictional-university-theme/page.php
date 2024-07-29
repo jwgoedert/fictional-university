fa7cb056fcdaf7d4ca2080fde1b4d32df51d9051
James Wesley Goedert <jwgoedert@gmail.com>
2024-07-29 23:26:10 +0200

    Add link side menu with conditional for parents and children

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -17,21 +17,36 @@ while (have_posts()) {
     if ($the_parent) { ?>
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="<?php echo get_permalink($the_parent) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($the_parent) ?></a> <span class="metabox__main"><?php the_title() ?></span>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink($the_parent) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($the_parent); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
         </p>
       </div>
     <?php }
     ?>
-    <!-- comment out page links below -->
-    <!--
-      <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
-        <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
-        </ul>
-      </div>
-    -->
+
+    <?php
+      $test_array = get_pages(array(
+        'child_of' => get_the_id()
+      ));
+      if($the_parent or $test_array) { ?>
+    
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($the_parent)?>"><?php echo get_the_title($the_parent)?></a></h2>
+      <ul class="min-list">
+        <?php
+          if ($the_parent) {
+            $find_children_of = $the_parent;
+          } else {
+            $find_children_of = get_the_id();
+          }
+          wp_list_pages(array(
+            'title_li' => null,
+            'child_of' => $find_children_of,
+            'sort_column' => 'menu_order'
+          )); ?>
+      </ul>
+    </div>
+    <?php } ?>
+    
     <div class="generic-content">
       <?php the_content(); ?>
     </div>
