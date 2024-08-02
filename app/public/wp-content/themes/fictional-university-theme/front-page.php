73e6c0fcaf8cf8239e6280fc89c13e71de73b79f
James Wesley Goedert <jwgoedert@gmail.com>
2024-08-03 01:33:15 +0200

    Change front page dates to display all events by ascending date

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -16,8 +16,11 @@
       <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
       <?php
       $home_page_events = new WP_Query(array(
-        'posts_per_page' => 2,
-        'post_type' => 'event'
+        'posts_per_page' => -1,
+        'post_type' => 'event',
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value_num',
+        'order' => 'ASC',
       ));
       while ($home_page_events->have_posts()) {
         $home_page_events->the_post(); ?>
