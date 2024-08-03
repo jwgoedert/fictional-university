eeee220b0c18a896736b03d0f5d11ac49a3c28a6
James Wesley Goedert <jwgoedert@gmail.com>
2024-08-03 03:52:09 +0200

    Create past events page from archive events and add nav between the two

--- app/public/wp-content/themes/fictional-university-theme/page-past-events.php
+++ app/public/wp-content/themes/fictional-university-theme/page-past-events.php
@@ -6,7 +6,7 @@ get_header();
 <div class="page-banner">
 	<div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
 	<div class="page-banner__content container container--narrow">
-		<h1 class="page-banner__title">All Events.</h1>
+		<h1 class="page-banner__title">Past Events.</h1>
 		<div class="page-banner__intro">
 			<p>See what's happening on our planet.</p>
 		</div>
@@ -15,8 +15,24 @@ get_header();
 
 <div class="container container--narrow page-section">
 	<?php
-	while (have_posts()) {
-		the_post();
+	$today = date('Y-m-d H:i:s');
+	$past_events = new WP_Query(array(
+		'paged' => get_query_var('paged', 1),
+		'post_type' => 'event',
+		'meta_key' => 'event_date',
+		'orderby' => 'meta_value',
+		'order' => 'ASC',
+		'meta_query' => array(
+			array(
+				'key' => 'event_date',
+				'compare' => '<',
+				'value' => $today,
+				'type' => 'DATETIME',
+			),
+		),
+	));
+	while ($past_events->have_posts()) {
+		$past_events->the_post();
 	?>
 		<div class="event-summary">
 			<a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
@@ -36,9 +52,12 @@ get_header();
 			</div>
 		</div>
 	<?php }
-	echo paginate_links();
+	echo paginate_links(array(
+		'total' => $past_events->max_num_pages,
+	));
 	?>
-	<p>Looking for a recap of past events?<a href="<?php echo site_url('/past-events')?>">  Check out our past events archive!</a></p>
+	<hr class="section-break">
+	<p><a href="<?php echo site_url('/events') ?>"> Current Events</a></p>
 </div>
 <?php
 get_footer();
