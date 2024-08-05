81889e39312cfb9665187d651b80919f800ba8df
James Wesley Goedert <jwgoedert@gmail.com>
2024-08-05 16:15:39 +0200

    add header and conditional for program pages and related events

--- app/public/wp-content/themes/fictional-university-theme/single-program.php
+++ app/public/wp-content/themes/fictional-university-theme/single-program.php
@@ -20,6 +20,67 @@ while (have_posts()) {
     <div class="generic-content">
       <?php the_content(); ?>
     </div>
+    <!-- Display next two upcoming events related to program -->
+    <?php
+      $today = date('Y-m-d H:i:s');
+      $home_page_events = new WP_Query(array(
+        'posts_per_page' => 2,
+        'post_type' => 'event',
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value',
+        'order' => 'ASC',
+        // Only return events that are greater than or equal to today's date
+        'meta_query' => array(
+          array(
+            'key' => 'event_date',
+            'compare' => '>=',
+            'value' => $today,
+            'type' => 'DATETIME',
+          ),
+          // Only return events that are related to the current program
+          array(
+            'key' => 'related_programs',
+            'compare' => 'LIKE',
+            'value' => '"' . get_the_ID() . '"'
+          )
+        ),
+      ));
+
+      if($home_page_events->have_posts()){
+        echo '<hr class="section-break">';
+      echo '<h2 class="headline headline--medium ">Upcoming ' . get_the_title() . ' Events</h2>';
+      while ($home_page_events->have_posts()) {
+        $home_page_events->the_post(); ?>
+        <div class="event-summary">
+          <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+            <span class="event-summary__month">
+              <?php
+              $event_date = new DateTime(get_field('event_date'));
+              echo $event_date->format('M') ?>
+            </span>
+            <span class="event-summary__day">
+              <?php
+              echo $event_date->format('d')
+              ?></span>
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h5>
+            <p><?php
+                the_field('event_date');
+                if (has_excerpt()) {
+                  echo get_the_excerpt();
+                } else {
+                  echo wp_trim_words(get_the_content(), 18);
+                }
+                ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+          </div>
+        </div>
+      <?php
+      }
+
+      }
+      ?>
+  </div>
   <?php }
 get_footer();
   ?>
