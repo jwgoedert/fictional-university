29dc70a0d05e65a153bbab8adce84b7bf518f92e
James Wesley Goedert <jwgoedert@gmail.com>
2024-08-06 00:15:30 +0200

    update single professor banner image

--- app/public/wp-content/themes/fictional-university-theme/single-professor.php
+++ app/public/wp-content/themes/fictional-university-theme/single-professor.php
@@ -4,7 +4,7 @@ while (have_posts()) {
   <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php 
     $page_banner_image = get_field('page_banner_background_image');
-    echo $page_banner_image['url']; ?>)"></div>
+    echo $page_banner_image['sizes']['pageBanner']; ?>)"></div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title(); ?></h1>
       <div class="page-banner__intro">
