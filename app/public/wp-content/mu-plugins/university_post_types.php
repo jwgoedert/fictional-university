60b478d108b10afd87fb6bde79c710df7f9932a9
James Wesley Goedert <jwgoedert@gmail.com>
2024-08-09 19:25:47 +0200

    Add Campus Post Type

--- app/public/wp-content/mu-plugins/university_post_types.php
+++ app/public/wp-content/mu-plugins/university_post_types.php
@@ -1,22 +1,23 @@
 <?php
 
-function university_post_types () {
-    // Event Post Type
-    register_post_type('event', array(
-        'supports' => array('title', 'editor', 'excerpt','custom-fields'),
-        'rewrite' => array('slug' => 'events'),
+function university_post_types()
+{
+    // Campus Post Type
+    register_post_type('campus', array(
+        'supports' => array('title', 'editor', 'excerpt', 'custom-fields'),
+        'rewrite' => array('slug' => 'campuses'),
         'has_archive' => true,
         'public' => true,
         'show_in_rest' => true,
         'labels' => array(
-            'name' => 'Events',
-            'add_new_item' => 'Add New Event',
-            'add_new' => 'Add New Event',
-            'edit_item' => 'Edit Event',
-            'all_items' => 'All Events',
-            'singular_name' => 'Event'
+            'name' => 'Campuses',
+            'add_new_item' => 'Add New Campus',
+            'add_new' => 'Add New Campus',
+            'edit_item' => 'Edit Campus',
+            'all_items' => 'All Campuses',
+            'singular_name' => 'Campus'
         ),
-        'menu_icon' => 'dashicons-calendar'
+        'menu_icon' => 'dashicons-location-alt'
     ));
 
     // Program Post Type    
@@ -37,20 +38,38 @@ function university_post_types () {
         'menu_icon' => 'dashicons-awards'
     ));
 
-        // Professor Post Type    
-        register_post_type('professor', array(
-            'supports' => array('title', 'editor', 'thumbnail'),
-            'public' => true,
-            'show_in_rest' => true,
-            'labels' => array(
-                'name' => 'Professors',
-                'add_new_item' => 'Add New Professor',
-                'add_new' => 'Add New Professor',
-                'edit_item' => 'Edit Professor',
-                'all_items' => 'All Professors',
-                'singular_name' => 'Professor'
-            ),
-            'menu_icon' => 'dashicons-welcome-learn-more'
-        ));
+    // Professor Post Type    
+    register_post_type('professor', array(
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'public' => true,
+        'show_in_rest' => true,
+        'labels' => array(
+            'name' => 'Professors',
+            'add_new_item' => 'Add New Professor',
+            'add_new' => 'Add New Professor',
+            'edit_item' => 'Edit Professor',
+            'all_items' => 'All Professors',
+            'singular_name' => 'Professor'
+        ),
+        'menu_icon' => 'dashicons-welcome-learn-more'
+    ));
+
+    // Event Post Type
+    register_post_type('event', array(
+        'supports' => array('title', 'editor', 'excerpt', 'custom-fields'),
+        'rewrite' => array('slug' => 'events'),
+        'has_archive' => true,
+        'public' => true,
+        'show_in_rest' => true,
+        'labels' => array(
+            'name' => 'Events',
+            'add_new_item' => 'Add New Event',
+            'add_new' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event'
+        ),
+        'menu_icon' => 'dashicons-calendar'
+    ));
 }
 add_action('init', 'university_post_types');
