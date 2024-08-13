9bb3dddcbb5dde500ea5dcf808cee9a0485339e3
James Wesley Goedert <jwgoedert@gmail.com>
2024-08-14 01:32:45 +0200

    add formatting to index.js

--- app/public/wp-content/themes/fictional-university-theme/src/index.js
+++ app/public/wp-content/themes/fictional-university-theme/src/index.js
@@ -5,8 +5,12 @@ import GoogleMap from "./modules/GoogleMap"
 import MobileMenu from "./modules/MobileMenu"
 import HeroSlider from "./modules/HeroSlider"
 import Search from "./modules/Search"
+
 // Instantiate a new object using our modules/classes
+
 const mobileMenu = new MobileMenu()
 const heroSlider = new HeroSlider()
 const googleMap = new GoogleMap()
 const search = new Search()
+
+
