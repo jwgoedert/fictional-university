3dd3da534d73b69d0fb6b728cb2075f6eb455d8e
James Wesley Goedert <jwgoedert@gmail.com>
2024-08-13 22:51:32 +0200

    import and instantiate GoogleMaps module

--- app/public/wp-content/themes/fictional-university-theme/src/index.js
+++ app/public/wp-content/themes/fictional-university-theme/src/index.js
@@ -1,9 +1,11 @@
 import "../css/style.scss"
 
 // Our modules / classes
+import GoogleMap from "./modules/GoogleMap"
 import MobileMenu from "./modules/MobileMenu"
 import HeroSlider from "./modules/HeroSlider"
 
 // Instantiate a new object using our modules/classes
 const mobileMenu = new MobileMenu()
 const heroSlider = new HeroSlider()
+const googleMap = new GoogleMap()
