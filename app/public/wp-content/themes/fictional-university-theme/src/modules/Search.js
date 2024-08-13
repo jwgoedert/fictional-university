94d83f5527bdcaba2a286113c38f6c5e930814a1
James Wesley Goedert <jwgoedert@gmail.com>
2024-08-14 01:43:37 +0200

    write dummy code to explain describe, events, methods sections of class constructor

--- app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -1,7 +1,27 @@
 class Search {
+  // 1. Describe and create/initiate our object
   constructor() {
-    alert('Search class is loaded');
+    this.name = 'James';
+    this.age = 38;
+    this.head = {};
+    this.brain = {};
   }
+  // 2. Add events
+   //on this.head feels cold, wearsHat
+   //on this.head feels warm, takesHatOff
+
+  
+  // 3. Methods
+
+  //wearHat()..
+  //takeHatOff()..
+  openOverlay() {
+    alert('Search overlay opened');
+  }
+
+  closeOverlay() {
+    alert('Search overlay closed');
+  } 
 }
 
 export default Search;
