dcb7a9982191f2e8bbe07bfa4d2c2a2070372f28
James Wesley Goedert <jwgoedert@gmail.com>
2024-08-14 03:08:51 +0200

    handle spinner logic for deleting text

--- app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -13,6 +13,7 @@ class Search {
     this.isOverlayOpen = false;
     this.isSpinnerVisible = false;
     this.typingTimer;
+    this.previousValue;
   }
   // 2. Add events
 
@@ -22,20 +23,32 @@ class Search {
     // Use keys to open and close overlay
     $(document).on('keydown', this.keyPressDispatcher.bind(this));
     // this direct selection is slower than JS refactor
-    this.searchField.on('keydown', this.typingLogic.bind(this));
+    this.searchField.on('keyup', this.typingLogic.bind(this));
   }
 
   // 3. Methods
 
   typingLogic() {
-    clearTimeout(this.typingTimer);
-    if (!this.isSpinnerVisible) {
-      this.resultsDiv.html('<div class="spinner-loader"></div>');
-      this.isSpinnerVisible = true;
+    if (this.searchField.val() != this.previousValue) {
+
+      clearTimeout(this.typingTimer);
+      if (this.searchField.val()) {
+        // if there is a value in the search field
+        if (!this.isSpinnerVisible) {
+          this.resultsDiv.html('<div class="spinner-loader"></div>');
+          this.isSpinnerVisible = true;
+        }
+        this.typingTimer = setTimeout(
+          this.getResults.bind(this)
+          , 2000);
+
+      } else {
+        // if there is no value in the search field
+        this.resultsDiv.html('');
+        this.isSpinnerVisible = false
+      }
     }
-    this.typingTimer = setTimeout(
-      this.getResults.bind(this)
-      , 2000);
+    this.previousValue = this.searchField.val();
   }
 
   getResults() {
