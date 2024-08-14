64b5606a521566194a7a17437a7cea6cc1b91da2
James Wesley Goedert <jwgoedert@gmail.com>
2024-08-14 02:00:15 +0200

    add no scroll for overlay

--- app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -20,10 +20,13 @@ class Search {
 
   openOverlay() {
     this.searchOverlay.addClass('search-overlay--active');
+    $('body').addClass('body-no-scroll');
   }
 
   closeOverlay() {
     this.searchOverlay.removeClass('search-overlay--active');
+    $('body').removeClass('body-no-scroll');
+
   } 
 }
 
