ddc889c8168a7989cdc3f2ce0dd3fe07e8b5e32c
James Wesley Goedert <jwgoedert@gmail.com>
2024-09-27 12:19:20 -0500

    Create test array for search results with template literals

--- app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -52,9 +52,17 @@ class Search {
   }
 
   getResults() {
-    $.getJSON('http://fictional-university.local/wp-json/wp/v2/posts?search=' + this.searchField.val(), function (posts) {
-      alert(posts[0].title.rendered);
-  }
+    $.getJSON('http://fictional-university.local/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+      let testArray =['red', 'orange', 'yellow'];
+      console.log('posts', posts);
+      this.resultsDiv.html(`
+        <h2 class="search-overlay__section-title">General Information:</h2>
+        <ul class="link-list min-list">
+          ${testArray.map(item => `<li>${item}</li>`).join('')}
+          <li><a href="${posts[0]? posts[0].link :'#;'}"> ${posts[0].title.rendered}</a></li>
+        </ul>
+        `);
+    }
     )
   }
   openOverlay() {
